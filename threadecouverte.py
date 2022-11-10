0cd208ffe48f0603d46e958b4ed6690ab36e591e
sephirz <soulwapx@gmail.com>
2022-11-10 08:40:43 +0100

    premiers exos

--- threadecouverte.py
+++ threadecouverte.py
@@ -1,5 +1,6 @@
 import threading
 import time
+
 def task(i):
     print(f"Task {i} starts")
     time.sleep(1)
@@ -7,7 +8,22 @@ def task(i):
 
 start = time.perf_counter()
 t1 = threading.Thread(target=task, args=[1])
-t1.start()
-t1.join()
+t2 = threading.Thread(target=task, args=[2])
+t1.start() # lancement des deux threads en meme temps (a peu près)
+t2.start()
+t1.join() # fin des deux threads
+t2.join()
 end = time.perf_counter()
 print(f"Tasks ended in {round(end - start, 2)} second(s)")
+
+# Test avec 50 Threads
+
+T = []
+for i in range(50):
+    T.append(threading.Thread(target=task, args=[i]))
+
+for i in range(len(T)):
+    T[i].start()
+
+for i in range(len(T)):
+    T[i].join()
