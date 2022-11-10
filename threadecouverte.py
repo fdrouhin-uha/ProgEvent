da75b99eda9e7550f2e928c1e1d1ce4095fb3b52
sephirz <soulwapx@gmail.com>
2022-11-10 08:46:24 +0100

    premiers exos

--- threadecouverte.py
+++ threadecouverte.py
@@ -1,29 +1,31 @@
 import threading
 import time
 
-def task(i):
-    print(f"Task {i} starts")
-    time.sleep(1)
-    print(f"Task {i} ends")
 
-start = time.perf_counter()
-t1 = threading.Thread(target=task, args=[1])
-t2 = threading.Thread(target=task, args=[2])
-t1.start() # lancement des deux threads en meme temps (a peu près)
-t2.start()
-t1.join() # fin des deux threads
-t2.join()
-end = time.perf_counter()
-print(f"Tasks ended in {round(end - start, 2)} second(s)")
+if __name__ == '__main__':
+    def task(i):
+        print(f"Task {i} starts")
+        time.sleep(1)
+        print(f"Task {i} ends")
 
-# Test avec 50 Threads
+    start = time.perf_counter()
+    t1 = threading.Thread(target=task, args=[1])
+    t2 = threading.Thread(target=task, args=[2])
+    t1.start() # lancement des deux threads en meme temps (a peu près)
+    t2.start()
+    t1.join() # fin des deux threads
+    t2.join()
+    end = time.perf_counter()
+    print(f"Tasks ended in {round(end - start, 2)} second(s)")
 
-T = []
-for i in range(50):
-    T.append(threading.Thread(target=task, args=[i]))
+    # Test avec 50 Threads
 
-for i in range(len(T)):
-    T[i].start()
+    T = []
+    for i in range(50):
+        T.append(threading.Thread(target=task, args=[i]))
 
-for i in range(len(T)):
-    T[i].join()
+    for i in range(len(T)):
+        T[i].start()
+
+    for i in range(len(T)):
+        T[i].join()
