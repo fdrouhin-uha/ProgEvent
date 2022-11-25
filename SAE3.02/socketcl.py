c2a41e7f4d2dcc28d008e856f171901283a7121a
sephirz <soulwapx@gmail.com>
2022-11-25 11:47:00 +0100

    rectification socketserv

--- SAE3.02/socketcl.py
+++ SAE3.02/socketcl.py
@@ -1,44 +1,32 @@
 import socket
 import threading
-machine1 = ['127.0.0.1',6824]
+host = '127.0.0.1'
+port = 6824
 client = socket.socket()
-
-
-def connexion(machine):
-    client.connect((machine[0],machine[1]))
-    t1 = threading.Thread(target=listen)
-    t2 = threading.Thread(target=write)
-    t1.start()
-    t2.start()
-    t1.join()
-    t2.join()
+client.connect((host, port))
 
 def listen():
     while True:
         data = client.recv(1024).decode()
         if not data:
             break
+        elif data == 'bye':
+            break
         print(' Reçu : ' + data)
     client.close()
 
 
-def write():
-    while True:
-        try:
-            data = input()
-            if data == "arret":
-                break
-            elif data =="disconnect":
-                client.send(data.encode())
-                client.close()
-                connexion(machine1)
-            client.send(data.encode())
-        except OSError:
-            print('Le socket n\'est pas connecté !')
-            co = str(input('Pour vous reconnecter tapper "yes" : '))
-            if co == "yes":
-                connexion(machine1)
-    client.close()
-
 if __name__ == '__main__':
-    connexion(machine1)
+    t1 = threading.Thread(target=listen)
+    t1.start()
+    data=''
+    try:
+        while data!='disconnect':
+            data = input()
+            client.send(data.encode())
+    except OSError:
+        print('Le socket n\'est pas connecté !')
+        co = str(input('Pour vous reconnecter tapper "yes" : '))
+        if co == "yes":
+            client.connect((host, port))
+    t1.join()
--- SAE3.02/testsrv.py
+++ SAE3.02/testsrv.py
@@ -1,55 +1,31 @@
 import socket
-import threading
 
 host = '127.0.0.1'
 port = 6824
 server = socket.socket()
 server.bind((host, port))
 server.listen(2)
-conn, address = server.accept()
 
-def listen():
+def server_program():
+    conn, address = server.accept()
+    print("Connection depuis : "+str(address))
     while True:
         data = conn.recv(1024).decode()
         if not data:
-            break
-        elif data == 'disconnect':
             conn.close()
-            print('Client déconnecté')
-            connexion()
-        elif data == 'askOS':
-            data = 'cmd ok'
-            conn.send(data.encode())
-        elif data == 'askRAM':
-            data = 'cmd ok'
-            conn.send(data.encode())
-        elif data == 'askCPU':
-            data = 'cmd ok'
-            conn.send(data.encode())
-        elif data == 'askIP':
-            data = 'cmd ok'
-            conn.send(data.encode())
-        elif data == 'askNAME':
-            data = 'cmd ok'
-            conn.send(data.encode())
-        print(' Reçu : ' + data)
-    conn.close()
-
-def connexion():
-    t1 = threading.Thread(target=listen)
-    t2 = threading.Thread(target=write)
-    t1.start()
-    t2.start()
-    t1.join()
-    t2.join()
-    conn.close()
-
-def write():
-    while True:
-        data = input()
-        if data == "arret":
+            print('Utilisateur deconnecté, retour à l\'écoute')
+            conn, address = server.accept()
+        elif data =="disconnect":
+            conn.close()
+            print('Utilisateur deconnecté, retour à l\'écoute')
+            conn, address = server.accept()
+        elif data =="kill":
+            break
+        elif data =='reset':
             server.close()
-        conn.send(data.encode())
+            server_program()
+        print("Reçu d'un utilisateur connecté : "+str(data))
+    server.close()
 
 if __name__ == '__main__':
-    connexion()
+    server_program()
