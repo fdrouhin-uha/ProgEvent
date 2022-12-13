da61cbad11e103782b46e3ffa61ca073c57e7422
sephirz <soulwapx@gmail.com>
2022-12-13 17:27:14 +0100

    oeee

--- SAE TEST/client.py
+++ SAE TEST/client.py
@@ -1,7 +1,10 @@
 import socket, threading, sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
-
+y = []
+ip = open("fich.txt", "r")
+for x in ip:
+  y.append(str(x).replace("\n", ""))
 
 class Client():
     def __init__(self, host, port):
@@ -14,7 +17,10 @@ class Client():
         try:
             self.__sock.connect((self.__host, self.__port))
         except ConnectionRefusedError:
-            print("Serveur non lancé ou mauvaises informations")
+            msg2 = QMessageBox()
+            msg2.setWindowTitle('Erreur')
+            msg2.setText('Serveur déconnecté ou mauvaises données !')
+            msg2.exec_()
             return -1
         except ConnectionError:
             print("Erreur de connexion")
@@ -24,11 +30,7 @@ class Client():
             return 0
 
     def envoi(self,msg):
-        try:
-            self.__sock.send(msg.encode())
-        except BrokenPipeError:
-            print("erreur, socket fermé")
-        return msg
+        self.__sock.send(msg.encode())
 
     def __reception(self, conn):
         msg = ""
@@ -47,39 +49,50 @@ class MainWindow(QMainWindow):
         grid = QGridLayout()
         widget.setLayout(grid)
         lab = QLabel("Connexion (IP - PORT)")
+        lab2 = QLabel("Liste des machines : ")
         self.__host = QLineEdit()
-        self.__port = QLineEdit()
+        self.__port = QLineEdit("6824")
         self.__msgcmd = QLineEdit()
         ok = QPushButton("Connexion")
         snd = QPushButton('Test')
+        self.__select = QComboBox()
+        self.__select.addItem(y[0])
+        self.__select.addItem(y[1])
+        self.__select.addItem(y[2])
         self.__result = QLabel("")
         quit = QPushButton("Quitter")
-        grid.addWidget(lab, 0, 0)
-        grid.addWidget(self.__host, 0, 1)
-        grid.addWidget(self.__port, 0, 2)
-        grid.addWidget(snd,1,0)
-        grid.addWidget(ok, 0, 3)
-        grid.addWidget(quit, 3, 0)
+        grid.addWidget(lab2,0, 0)
+        grid.addWidget(lab, 1, 0)
+        grid.addWidget(self.__host, 1, 1)
+        grid.addWidget(self.__port, 1, 2)
+        grid.addWidget(self.__select, 0, 1)
+        grid.addWidget(ok, 1, 3)
+        grid.addWidget(quit, 4, 0)
+        self.combobox()
         quit.clicked.connect(self.actionQuitter)
         ok.clicked.connect(self.initcon)
-        snd.clicked.connect(self.sendmsg)
+        self.__select.activated.connect(self.combobox)
         self.setWindowTitle("Monitoring")
+
+
+    def combobox(self):
+        if self.__select.currentText() == y[0]:
+            self.__host.setText(y[0])
+        elif self.__select.currentText() == y[1]:
+            self.__host.setText(y[1])
+        else:
+            self.__host.setText(y[2])
+
 
     def initcon(self):
         client=Client(str(self.__host.text()), int(self.__port.text()))
         try:
             client.connect()
-        except OSError:
+        except:
             msg2 = QMessageBox()
             msg2.setWindowTitle('Erreur')
-            msg2.setText('Serveur déconnecté ou mauvaises données !')
+            msg2.setText('Erreur de connexion')
             msg2.exec_()
-        return client
-
-    def sendmsg(self):
-        cl=self.initcon
-
-
 
 
 
@@ -92,5 +105,5 @@ if __name__ == "__main__":
     window.show()
     app.exec()
 
-
+# fichier : lire (contient differenttes ip serveur) il faut lire le fichier (on peut utiliser un open pr choisir le fichier. Lister les serveurs sur le cote
 
